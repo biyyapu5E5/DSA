6e7027685526999b4803522b5101459375d92307
biyyapu5E5 <78197963+biyyapu5E5@users.noreply.github.com>
2025-07-27 19:00:08 +0530

    Create linkedListIE.py

---
+++ linkedListIE.py
@@ -0,0 +1,34 @@
+#created a Linked list insert from end
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        
+class LinkedList:
+    def __init__(self):
+        self.head = None 
+    
+    def appendStart(self, data):
+        new_node = Node(data)
+        
+        if self.head is None:
+            self.head = new_node
+        else:
+            current = self.head 
+            new_node.next = current
+            self.head = new_node
+    
+    def print(self):
+        current = self.head
+        while current:
+            print(current.data, end = ' --> ')
+            current = current.next 
+        print('null')
+        
+ll = LinkedList()
+ll.appendStart(10)
+ll.appendStart(20)
+ll.appendStart(30)
+ll.appendStart(40)
+ll.appendStart(50)
+ll.print()
